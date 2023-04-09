3a2922757fb3c3cbaf339b611f0edba49f877649
@arielf02 <ariel.febrian@bankraya.co.id>
2023-04-09 17:27:08 +0700

    feat: add userForm constant

---
+++ src/constants/usersForm.ts
@@ -0,0 +1,22 @@
+import { SelectAbleType } from "@component/components/SelectComponent";
+
+export const status: SelectAbleType[] = [
+  {
+    label: "Active",
+    value: "active",
+  },
+  {
+    label: "Inactive",
+    value: "inactive",
+  },
+];
+export const gender: SelectAbleType[] = [
+  {
+    label: "Male",
+    value: "male",
+  },
+  {
+    label: "Female",
+    value: "Female",
+  },
+];
